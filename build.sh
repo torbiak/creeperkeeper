c238706b519cc6e4259b7e47b5735ddb0139e7e9
Jordan Torbiak <torbiak@gmail.com>
2017-01-05 03:28:50 -0700

    Fix binary extension in release archives for windows

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# Create archives containing the crkr binary for the target platforms. Don't
+# run on cygwin, since it's difficult to get the extension right for the
+# archives for windows.
+set -e
 
 function build_all {
     local version=${1:?No version given}; shift
@@ -14,8 +18,12 @@ function build {
     local os=${1:?No os given}; shift
     local arch=${1:?No arch given}; shift
     GOOS=$os GOARCH=$arch go build -ldflags "-X main.version=$version" ./cmd/crkr
-    zip -q crkr-v$version-$os-$arch.zip crkr
-    rm crkr
+    local bin=crkr
+    if test $os = windows; then
+        bin=${bin}.exe
+    fi
+    zip -q crkr-v$version-$os-$arch.zip $bin
+    rm $bin
 }
 
 build_all "$@"
